941ecd8b21ae5a12cb06b6559e4942bea02c3a82
yuan1998 <chizhiyueshu@gmail.com>
2025-01-18 17:49:21 +0800

    first commit

---
+++ app/Admin/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Administrator;
+use Dcat\Admin\Admin;
+use Dcat\Admin\Form;
+use Dcat\Admin\Grid;
+use Dcat\Admin\Layout\Column;
+use Dcat\Admin\Layout\Content;
+use Dcat\Admin\Layout\Row;
+
+class UserController extends \Dcat\Admin\Http\Controllers\UserController
+{
+}
